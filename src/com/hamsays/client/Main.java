f8d48a74f47def92db90399c15765962b8d0d013
Daniel Harris <dan_harris10@yahoo.com>
2022-02-21 08:36:04 -0800

    HAMApp methods, skeleton, and instantiation in Main

--- src/com/hamsays/client/Main.java
+++ src/com/hamsays/client/Main.java
@@ -1,7 +1,10 @@
 package com.hamsays.client;
 
+import com.hamsays.controller.HAMApp;
+
 class Main {
     public static void main(String[] args) {
-
+        HAMApp app = new HAMApp();
+        app.execute();
     }
 }
--- src/com/hamsays/controller/HAMApp.java
+++ src/com/hamsays/controller/HAMApp.java
@@ -1,31 +1,100 @@
 package com.hamsays.controller;
 
+import com.apps.util.Console;
+import com.apps.util.Prompter;
 import com.hamsays.Board;
+import com.hamsays.Colors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
-class HAMApp {
+public class HAMApp {
     private Board board = new Board();
+    Prompter prompter = new Prompter(new Scanner(System.in));
     
     public void execute() {
         welcome();
         promptForName();
-        startGame();
-        displayColor();
+        startGame(promptForName());
         promptForColor();
+        gameOver(promptForName());
     }
 
-    private void promptForColor() {
+
+    private void welcome() {
+        Console.blankLines(2);
+        System.out.println("W E L C O M E  T O  H A M  S A Y S ! ! !");
+        Console.blankLines(2);
+    }
+
+    private String promptForName() {  //what will we do with name?
+        String name = prompter.prompt("Please enter your name: ",
+                "[a-zA-Z]*( [a-zA-Z]*)?", "Please restrict name input to alphabetical characters");
+        return name;
+    }
+
+    private void startGame(String name) {
+        System.out.println("Let's begin " + name);
+
+        displayColor();
     }
 
     private void displayColor() {
+        //access random color array from Board
+        //board.show();
     }
 
-    private void startGame() {
+    private List<Colors> promptForColor() {
+        List<Colors> colorsList = new ArrayList<>();
+
+        String error = "Please choose between one of the four colors: "
+                + "[" + Colors.BLUE + ", "
+                + Colors.RED + ","
+                + Colors.GREEN + ","
+                + Colors.YELLOW + "]";
+
+        String colors = prompter.prompt("Please enter the color(s) " +
+                "that were displayed: ", "(G|R|B|Y)*?",
+                error).toUpperCase(Locale.ROOT);
+
+        char [] color = colors.toCharArray();
+
+        for (char c : color) {
+            switch (c) {
+                case 'G':
+                    colorsList.add(Colors.GREEN);
+                    break;
+                case 'R':
+                    colorsList.add(Colors.RED);
+                    break;
+                case 'B':
+                    colorsList.add(Colors.BLUE);
+                    break;
+                case 'Y':
+                    colorsList.add(Colors.YELLOW);
+                    break;
+                default:
+                    System.out.println(colorsList);
+                    break;
+
+        }
+        }
+        System.out.println(colorsList);
+        return colorsList;
     }
 
-    private void promptForName() {
+
+    private void gameOver(String name) {
+        // if promptForColor != displayColor game will end
+        // if reach success count we congratulate
     }
 
-    private void welcome() {
-    }
+
+
+
+
+
+
 
 }
