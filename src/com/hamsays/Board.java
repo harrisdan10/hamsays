52c35e9ae6cc8f44d1147f618cd29acced53f831
Daniel Harris <dan_harris10@yahoo.com>
2022-02-24 08:23:50 -0800

    replay logic, thanks for playing, exception handling

--- src/com/hamsays/Board.java
+++ src/com/hamsays/Board.java
@@ -8,22 +8,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
-public abstract class Board extends Thread{
+public abstract class Board extends Thread {
     private final List<Color> colorList = new ArrayList<>();
     private List<Color> playerColorInput;
-    private boolean gameOver = false;
+    public boolean gameOver = false;
     Prompter prompter = new Prompter(new Scanner(System.in));
 
     public abstract void display();
 
     void getRandomColor() {
-            int color = new Random().nextInt(Color.values().length);
-            Color newColor = Color.values()[color];
-            colorList.add(newColor);
-            System.out.println(colorList);
+        int color = new Random().nextInt(Color.values().length);
+        Color newColor = Color.values()[color];
+        colorList.add(newColor);
+        System.out.println(colorList);
     }
 
-     void promptForColor() {
+    void promptForColor() {
         String error = "Please choose between one of the four colors: "
                 + "[" + Color.BLUE + ", "
                 + Color.RED + ","
@@ -33,45 +33,49 @@ public abstract class Board extends Thread{
         String colors = prompter.prompt("HAM Says the color sequence was? ", "(G|g|R|r|B|b|Y|y)*",
                 error);
 
-         addToPlayerColorInput(colors);
-     }
+        addToPlayerColorInput(colors);
+    }
 
-    public void gameSequence() {
-            int correctCount = 0;
-            display();
-            promptForColor();
-            while (!gameOver) {
-                if (colorList.equals(playerColorInput)) {
-                    correctCount++;
-                    if (correctCount == 10) {
-                        gameOver = true;
-                        Console.clear();
-                        Console.blankLines(2);
-                        try {
-                            String line = Files.readString(Path.of("data/win.txt"));
-                            System.out.println("\u001B[32m" + line + "\u001B[37m");
-                        } catch (IOException e) {
-                            System.out.println(e);
-                        }
-                        System.out.println("Good Job you got all " + correctCount + " correct");
-                    } else {
-                        display();
-                        promptForColor();
-                    }
-                } else {
+    public void gameSequence() throws IOException {
+        int correctCount = 0;
+
+        display();
+        promptForColor();
+        Console.clear();
+
+        while (!gameOver) {
+            if (colorList.equals(playerColorInput)) {
+                correctCount++;
+                if (correctCount == 10) {
                     gameOver = true;
                     Console.clear();
                     Console.blankLines(2);
-                    try {
-                        String line = Files.readString(Path.of("data/lose.txt"));
-                        System.out.println("\u001B[31m" + line + "\u001B[37m");
-                    } catch (IOException e) {
-                        System.out.println(e);
+                    if (Files.exists(Path.of("data/win.txt"))) {
+                        String line = Files.readString(Path.of("data/win.txt"));
+                        System.out.println("\u001B[32m" + line + "\u001B[37m");
+                    } else {
+                       throw new IOException("Please verify win.txt location");
                     }
-                    System.out.println("Sorry, you lose. You got " + correctCount + " correct.");
+                    System.out.println("Good Job you got all " + correctCount + " correct");
+                } else {
+                    display();
+                    promptForColor();
+                    Console.clear();
                 }
+            } else {
+                gameOver = true;
+                Console.clear();
+                Console.blankLines(2);
+                if (Files.exists(Path.of("data/lose.txt"))) {
+                    String line = Files.readString(Path.of("data/lose.txt"));
+                    System.out.println("\u001B[31m" + line + "\u001B[37m");
+                } else {
+                    throw new IOException("Please verify lose.txt location");
+                }
+                System.out.println("Sorry, you lose. You got " + correctCount + " correct.");
             }
         }
+    }
 
     private void addToPlayerColorInput(String colors) {
         playerColorInput = new ArrayList<>();
@@ -96,4 +100,5 @@ public abstract class Board extends Thread{
             }
         }
     }
+
 }
--- src/com/hamsays/controller/HAMApp.java
+++ src/com/hamsays/controller/HAMApp.java
@@ -9,7 +9,6 @@ import com.hamsays.Difficulty;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -18,32 +17,32 @@ public class HAMApp{
     private Difficulty level;
     Prompter prompter = new Prompter(new Scanner(System.in));
     
-    public void execute () {
+    public void execute () throws IOException {
         welcome();
         promptForName();
         Difficulty level = promptForDifficulty();
         board = BoardFactory.createBoard(level);
         Console.clear();
         startGame();
+        replayPrompt();
     }
 
 
-    private void welcome() {
+    private void welcome() throws IOException {
         Console.blankLines(2);
 
-        try {
-            String line = Files.readString(Path.of("data/welcome.txt"));
-            System.out.println("\u001B[34m" + line + "\u001B[37m");
-        } catch (IOException e) {
-            System.out.println(e);
+        if (Files.exists(Path.of("data/welcome.txt"))) {
+            String welcome = Files.readString(Path.of("data/welcome.txt"));
+            System.out.println("\u001B[34m" + welcome + "\u001B[37m");
+        } else {
+            throw new IOException("Please verify welcome.txt location");
         }
         Console.blankLines(2);
     }
 
-    private String promptForName() {  //what will we do with name?
+    private void promptForName() {  //what will we do with name?
         String name = prompter.prompt("What are your initials? up to 3 only please: ",
                 "[a-zA-Z]{1,3}", "Please restrict name input to alphabetical characters and a limit of 3");
-        return name;
     }
 
     private Difficulty promptForDifficulty() {
@@ -69,8 +68,36 @@ public class HAMApp{
         return level;
     }
 
-    private void startGame() {
+    private void startGame() throws IOException {
         board.gameSequence();
     }
 
+    private void replayPrompt() throws IOException {
+        String replay = prompter.prompt("Would you like to play again? Y/N?: ", "Y|y|N|n",
+                "Please select either Y or N");
+
+        replayGame(replay);
+    }
+
+    private void replayGame(String replay) throws IOException {
+        String inputConversion = replay.toUpperCase();
+            if ("Y".equals(inputConversion)){
+                Console.clear();
+                Difficulty level = promptForDifficulty();
+                board = BoardFactory.createBoard(level);
+                Console.clear();
+                startGame();
+                replayPrompt();
+            }
+            else {
+                Console.blankLines(2);
+                if (Files.exists(Path.of("data/thanks.txt"))){
+                    String thanks = Files.readString(Path.of("data/thanks.txt"));
+                    System.out.println("\u001B[33m" + thanks + "\u001B[37m");
+                } else {
+                    throw new IOException("Please verify thanks.txt location");
+                }
+                Console.blankLines(2);
+            }
+    }
 }
